9895ea2e6cb8b901d55150db7c31d705425371f9
Joeffison Silverio de Andrade <joeffison@gmail.com>
2018-09-15 23:02:24 +0200

    [SPOJ] ADDREV - Adding Reversed Numbers

---
+++ challenges/spoj/addrev/addrev_v001.py
@@ -0,0 +1,9 @@
+import fileinput
+
+if __name__ == '__main__':
+  f_in = fileinput.input()
+
+  n_test_cases = int(f_in.readline())
+
+  for test_case in range(n_test_cases):
+    print(int(str(sum(map(lambda x: int(x[::-1]), f_in.readline().split())))[::-1]))
